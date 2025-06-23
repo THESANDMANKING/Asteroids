5c75ce9acf45e2dcb45c3bddda54c0b561ff14d5
THESANDMANKING <ben.weinhagen@gmail.com>
2025-06-23 16:05:45 -0500

    Create Circle parent, Add clock/framerate logic

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+# cd github.com/Asteroids
+# source venv/bin/activate
+
 # this allows us to use code from the open-source pygame library throughout this file
 import pygame
 from constants import *
@@ -5,6 +8,11 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    # Creates a clock object
+    # this helps with the framerate and tracking the time in frames
+    clock = pygame.time.Clock()
+    # Variable being used for delta time
+    dt = 0
     while True:
         # If the X to close the window is clicked, the window closes
         for event in pygame.event.get():
@@ -12,6 +20,11 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        # Sets the framerate to 60 frames
+        clock.tick(60)
+        # Converts the milliseconds of frames to seconds
+        dt = clock.tick(60) / 1000
+
         
 
 
